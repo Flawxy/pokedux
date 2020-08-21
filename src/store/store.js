d42c89568c95d19844eec5f34891efccf491c6e3
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-08-21 12:12:19 +0200

    Create & Connect the Redux Store

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducer'
+import initialState from './initialState'
+
+const store = createStore(reducer, initialState)
+
+export default props => <Provider store={store} {...props} />
